43f4651202ec2d12371b76bdd151b45ce747b4c6
Glauber Carvalho <glaubercarv@gmail.com>
2020-09-15 14:26:36 -0300

    Start development

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: React.FC = () => (
+  <View>
+    <Text>Component App</Text>
+  </View>
+);
+
+export default App;
