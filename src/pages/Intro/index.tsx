389c164970eca473891068f4b374da6fbcd9d4c3
Glauber Carvalho <glaubercarv@gmail.com>
2020-09-15 15:12:50 -0300

    Add React Navigation lib

---
+++ src/pages/Intro/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Intro: React.FC = () => (
+  <View>
+    <Text>Component Intro</Text>
+  </View>
+);
+
+export default Intro;
